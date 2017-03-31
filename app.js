1c3ad6ac3abdee4fe3a2dc71cab1b1eb8a1d7408
lujianan <ljn6176@163.com>
2017-03-31 13:02:38 +0800

    add servers/web

--- app.js
+++ app.js
@@ -1,53 +1,8 @@
 'use strict';
 
-const http = require('http');
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-
 require('./global_regist');
-const index = require('./routes/index');
-const users = require('./routes/users');
+const web = require('./servers/web');
 
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger.log4js.connectLogger(logger, config.log));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-let server = http.createServer(app);
-
-server.listen(config.web.port, function () {
-  logger.info(config.web.name, config.web.url, 'start up');
+Promise.resolve([web]).each(function (app) {
+  app.start();
 });
