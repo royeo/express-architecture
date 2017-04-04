c3ec31c1bf3263b1b9d84529e9b72dea78c3ad5a
LemonSprite <ljn6176@163.com>
2017-04-04 21:06:21 +0800

    test ioredis

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,21 @@
 
 let express = require('express');
 let router = express.Router();
+let cache = require('../lib/cache/cache');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  console.log('------------------------');
-  res.render('index', {title: 'Express'});
+  cache.get('key').then(result => {
+    if (result) {
+      res.send('welcome again');
+    } else {
+      cache.set('key', 'value', 10).then(() => {
+        res.send('welcome first');
+      });
+    }
+  }).catch((err) => {
+    console.log(err.message);
+  });
 });
 
 module.exports = router;
