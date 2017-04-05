93a6a42f2b0ca5e475d5c6879bde33ea502e9a2b
lujianan <ljn6176@163.com>
2017-04-05 16:10:58 +0800

    disable x-powered-by

--- servers/web.js
+++ servers/web.js
@@ -17,6 +17,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+app.disable('x-powered-by');
 
 app.use(compression());
 app.use(responseTime());
