c6827217b7deb21a9e953ffe1c486a9038b81efe
lujianan <ljn6176@163.com>
2017-04-05 15:47:17 +0800

    add compression and response-time

--- servers/web.js
+++ servers/web.js
@@ -1,27 +1,32 @@
 'use strict';
 
 const http = require('http');
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+const responseTime = require('response-time');
+const compression = require('compression');
 
 const router = require('../routes');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression());
+app.use(responseTime());
 app.use(logger.log4js.connectLogger(logger, config.log));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, '../public/favicon.ico')));
 
 app.use(router);
 
