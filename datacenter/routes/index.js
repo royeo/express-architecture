1f1f3bf135dd495b398b8413642b4c7e994c1299
lujianan <ljn6176@163.com>
2017-09-12 13:21:36 +0800

    add validator route

--- datacenter/routes/index.js
+++ datacenter/routes/index.js
@@ -2,13 +2,14 @@
 
 const express      = require('express');
 const createRouter = require('../tools/create-router');
+const validator    = require('../middlewares/param-validator');
 
-const router = express.Router();
+const router = module.exports = express.Router();
 
 const v1Web   = createRouter(path.join(__dirname, 'v1/web'));
 const v1Admin = createRouter(path.join(__dirname, 'v1/admin'));
 
+validator(router);
+
 router.use('/api/v1', v1Web);
 router.use('/adminapi/v1', v1Admin);
-
-module.exports = router;
